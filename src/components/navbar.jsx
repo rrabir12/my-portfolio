a54cb2f6479670560a2aa5f5baca618af4d039b6
Rabi Roy <rrabir9815@gmail.com>
2025-08-11 22:14:23 +0545

    few changes

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import './Navbar.css';
+import './navbar.css';
 import { links } from '../data';
 
 function Navbar() {
